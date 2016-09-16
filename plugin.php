c6dd856e44e935ca8246b73c068edb5445c9d3c9
Dwane Dunn <Dwane.Dunn@gmail.com>
2016-09-16 15:02:34 -0400

    Created index.php and plugin.php. Filled out PHP Doc Block

---
+++ plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Starter WordPress Plugin
+ *
+ * @package     KnowITMedia
+ * @author      Dwane Dunn
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Starter WordPress Plugin
+ * Plugin URI:  https://dwanedunn.com
+ * Description: Know IT Media Starter Plugin Template. Use this plugin to base future plugin development on.
+ * Version:     1.0.0
+ * Author:      Dwane Dunn
+ * Author URI:  https://dwanedunn.com
+ * Text Domain: knowitmedia
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+namespace KnowITMedia;
